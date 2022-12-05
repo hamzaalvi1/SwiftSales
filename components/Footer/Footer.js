53bf0f108193b9daa99ee7b59fd45c7b95a6d131
Hamza Alvi <42583422+hamzaalvi1@users.noreply.github.com>
2022-12-05 20:11:50 +0500

    Update Footer.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import styles from "./footer.module.scss"
+import styles from "./Footer.module.scss"
 import { Container, Row, Col } from "react-bootstrap"
 import Image from "next/image"
 import Link from "next/link"
